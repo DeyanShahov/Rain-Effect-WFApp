9c416473fa700005c58932f953fbcbf38a2bf055
redfoxbg <redfoxbg@gmail.com>
2023-09-08 23:14:15 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace Rain_Effect_WFApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TimerEvent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormPaintEvent(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Particles.cs
@@ -0,0 +1,29 @@
+﻿namespace Rain_Effect_WFApp
+{
+    internal class Particles
+    {
+        public int height;
+        public int width;
+        public int posX;
+        public int posY;
+        public int[] size = { 30, 40, 50};
+        Random rand = new Random();
+        public int speed = 0;
+        int i = 0;
+
+        public Image particle;
+
+        public Particles(string filelocation)
+        {
+            particle = Image.FromFile(filelocation);
+            i = rand.Next(0, size.Length);
+            height = size[i];
+            width = size[i];
+
+            posX = rand.Next(-200, 800);
+            posY = rand.Next(-700, -200);
+
+            speed = rand.Next(1, 10);
+        }
+    }
+}
